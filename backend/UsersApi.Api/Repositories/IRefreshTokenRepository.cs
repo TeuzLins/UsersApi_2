3d84b41898ee1f187a3c2d6e35d4712f74d8347b
Teuz <163695559+TeuzLins@users.noreply.github.com>
2026-02-02 18:59:19 -0300

    Create IRefreshTokenRepository.cs

---
+++ backend/UsersApi.Api/Repositories/IRefreshTokenRepository.cs
@@ -0,0 +1,10 @@
+using UsersApi.Api.Models;
+
+namespace UsersApi.Api.Repositories;
+
+public interface IRefreshTokenRepository
+{
+    Task<RefreshToken?> GetByTokenAsync(string token);
+    Task AddAsync(RefreshToken refreshToken);
+    Task UpdateAsync(RefreshToken refreshToken);
+}
